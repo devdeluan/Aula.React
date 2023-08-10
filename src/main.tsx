1a80bd595403fc13a24227d15bac35422b3c1732
Denis luan Sales de Santana <394.299.878-52@info.local>
2023-08-09 21:54:39 -0300

    ok1

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home'
 import ListaServicos from './pages/ListaServicos'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ListaServicos/>
+    <Header/>
+    <Home/>
+    <Footer/>
   </React.StrictMode>,
 )
