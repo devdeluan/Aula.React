93469c4070c5748ec609681578faaca015229f6f
Denis luan Sales de Santana <394.299.878-52@info.local>
2023-08-11 21:54:56 -0300

    ok1

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+
+
 // import App from './App'
 import './index.css'
 import Home from './pages/Home'
 import ListaServicos from './pages/ListaServicos'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ListaDevs from './pages/ListaDevs'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <BrowserRouter>
+
     <Header/>
-    <Home/>
+
+    <Routes>
+      <Route  path='/' element={ <Home/>}/>
+      <Route path='listaServico' element= {<ListaServicos/>}/>
+      <Route path='ListaDevs' element= {<ListaDevs/>}/>
+    </Routes>
+
     <Footer/>
+
+    </BrowserRouter>
   </React.StrictMode>,
 )
